fad6f041bc56e9132ecc971fae7102a7090b4903
kouosi <kouosi@tuta.io>
2025-04-19 18:59:44 +0545

    fix(theme): Fix flash of unstyled content on load

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,44 +1,54 @@
-/* Switch theme */
-function changeIcon() {
-    if (document.body.dataset.theme == 'light') {
-        themeSwitcher.innerHTML = "<i class='fa fa-sun'></i>";
-    } else {
-        themeSwitcher.innerHTML = "<i class='fa fa-moon'></i>";
-    }
-}
-const themeSwitcher = document.getElementById('theme-switcher');
-
-document.body.dataset.theme = localStorage.getItem('theme') || 'dark';
-changeIcon();
-
+"use strict";
+/* Giscus management */
 function sendMsg2Giscus(message) {
     const iframe = document.querySelector('iframe.giscus-frame');
     if (!iframe) return;
     iframe.contentWindow.postMessage({ giscus: message }, 'https://giscus.app');
 }
 
-themeSwitcher.addEventListener('click', () => {
-    const currentTheme = document.body.dataset.theme;
-    const newTheme = currentTheme === 'light' ? 'dark' : 'light';
+/* Theme management */
+const themeSwitcher = document.getElementById('theme-switcher');
+/* Change icon according to them */
+function changeIcon() {
+    const isLightMode = document.documentElement.getAttribute('data-theme') === 'light';
+    if (isLightMode) {
+        themeSwitcher.innerHTML = "<i class='fa fa-moon' title='Switch to dark mode'></i>";
+    } else {
+        themeSwitcher.innerHTML = "<i class='fa fa-sun' title='Switch to light mode'></i>";
+    }
+}
 
-    document.body.dataset.theme = newTheme;
+/* Add event to change theme */
+themeSwitcher.addEventListener('click', () => {
+    const isLightMode = document.documentElement.getAttribute('data-theme') === 'light';
+    const newTheme = isLightMode ? 'dark' : 'light';
+    if (!isLightMode) {
+        document.documentElement.setAttribute('data-theme', 'light');
+    } else {
+        document.documentElement.removeAttribute('data-theme');
+    }
     localStorage.setItem('theme', newTheme);
     changeIcon();
     sendMsg2Giscus({ setConfig: { theme: "gruvbox_" + newTheme } });
 });
 
+/* Set icon and comment theme on load */
+const currentTheme = localStorage.getItem('theme') || 'dark';
+changeIcon();
+sendMsg2Giscus({ setConfig: { theme: "gruvbox_" + currentTheme } });
+
+/* Navbar management */
+const dropdown_toggle = document.getElementById('dropdown-toggle');
+const nav_bar = document.querySelector('nav');
+
 /* Toggle navbar */
-document.getElementById('dropdown-toggle').addEventListener('click', function() {
-    document.querySelector('nav').classList.toggle('open');
+dropdown_toggle.addEventListener('click', () => {
+    nav_bar.classList.toggle('open');
 });
 
-/* Disable navbar on click */
-document.addEventListener('click', function(event) {
-    const nav = document.querySelector('nav');
-    const toggle = document.getElementById('dropdown-toggle');
-
-    // Check if the click is outside the navigation menu and the toggle button
-    if (!nav.contains(event.target) && !toggle.contains(event.target)) {
-        nav.classList.remove('open');
+/* Disable navbar */
+document.addEventListener('click', (e) => {
+    if (!nav_bar.contains(e.target) && !dropdown_toggle.contains(e.target)) {
+        nav_bar.classList.remove('open');
     }
 });
